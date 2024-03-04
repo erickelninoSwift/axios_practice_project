816de9cd98195547dd17b2622878e37bedda6028
Erick El nino <erick.tshimbombo@yahoo.com>
2024-03-04 20:01:37 +0200

    done with get methode using axios

--- src/examples/1-first-request.js
+++ src/examples/1-first-request.js
@@ -1,12 +1,23 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from "react";
+import axios from "axios";
 // limit, if 429 wait for 15 min and try again
-const url = 'https://course-api.com/react-store-products';
+const url = "https://course-api.com/react-store-products";
 
 const FirstRequest = () => {
+  const [allCourse, setAllCourse] = useState([]);
+
+  const getAllcourses = async () => {
+    try {
+      const response = await axios(url);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
   useEffect(() => {
-    console.log('first axios request');
+    getAllcourses();
   }, []);
 
-  return <h2 className='text-center'>first request</h2>;
+  return <h2 className="text-center">first request</h2>;
 };
 export default FirstRequest;
